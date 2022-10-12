a1afc0a98b7428c1d8a9b66cf7c4600a5fd2df25
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-12 19:05:41 +0530

    Implemented CRUD in UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
-import { useGetTodosQuery } from "./feature/api/apiSlice";
+import {
+  useAddTodoMutation,
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} from "./feature/api/apiSlice";
 
 function App() {
   const [todo, setTodo] = useState("");
@@ -12,10 +18,14 @@ function App() {
     error,
   } = useGetTodosQuery();
 
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (todo) {
-      // logic to add todo using RTK Query
+      addTodo({ title: todo, completed: false });
       setTodo("");
     }
   };
@@ -26,9 +36,14 @@ function App() {
   } else if (isSuccess) {
     content = todos.map((todo) => (
       <ul>
-        <li className={todo.completed ? "checked" : ""}>
+        <li
+          className={todo.completed ? "checked" : ""}
+          onClick={() => updateTodo({ ...todo, completed: !todo.completed })}
+        >
           {todo.title}
-          <span className="close">x</span>
+          <span className="close" onClick={() => deleteTodo({ id: todo.id })}>
+            x
+          </span>
         </li>
       </ul>
     ));
