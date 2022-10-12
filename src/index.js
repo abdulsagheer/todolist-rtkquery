9f63a378436daa08b87e8dcab34f767c862057cb
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-12 18:41:41 +0530

    Added Api Provider

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./feature/api/apiSlice";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ApiProvider api={apiSlice}>
       <App />
-    </Provider>
+    </ApiProvider>
   </React.StrictMode>
 );
-
